68b3c34df3a041dc6518258a0c1a418a230dea74
Daniel <danieldossantos1707@yahoo.com.br>
2017-09-22 10:25:21 +0100

    Changed the place that the test read. Now the test read the cucumber file

--- step_definitions/features/search.rb
+++ step_definitions/features/search.rb
@@ -1,17 +1,18 @@
 require 'rubygems'
 require './pages/google_page'
-require './core/Read_Setup'
+require './core/Read_setup'
 
 Given(/^We navigate to the Google home page/) do
   @Google = Google_page.new
   @Google.open_page
 end
 
-When(/^We search for the automação teste/) do
-  arquivo_setup = Setup.Read("C:/Users/Fenix/Desktop/RUBY/PROJETOS/step_definitions/Files/Setup.txt")
-  @Google.fill_search_bar("#{arquivo_setup['PESQUISAR']}")
+When(/^We search for (.*)$/) do |search_to_fill|
+  #arquivo_setup = Setup.Read("C:/Users/Fenix/Desktop/RUBY/PROJETOS/step_definitions/Files/Setup.txt")
+  @Google.fill_search_bar(search_to_fill)
+  @Google.click_pesquisa_google
 end
 
 Then(/^The results for the search will be displayed/) do
-  @Google.click_pesquisa_google
+  
 end
