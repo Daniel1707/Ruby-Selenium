63d3f21f01f933b0796c179ac0611236dbc55855
Daniel <danieldossantos1707@yahoo.com.br>
2017-09-24 12:11:15 +0100

    Added exception handling

--- step_definitions/features/search.rb
+++ step_definitions/features/search.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require './pages/google_page'
-require './core/Read_setup'
+#require './core/Read_setup'
 
 Given(/^We navigate to the Google home page/) do
   @Google = Google_page.new
@@ -10,9 +10,9 @@ end
 When(/^We search for (.*)$/) do |search_to_fill|
   #arquivo_setup = Setup.Read("C:/Users/Fenix/Desktop/RUBY/PROJETOS/step_definitions/Files/Setup.txt")
   @Google.fill_search_bar(search_to_fill)
-  @Google.click_pesquisa_google
+  @Google.submit
 end
 
 Then(/^The results for the search will be displayed/) do
-  
+
 end
--- step_definitions/pages/google_page.rb
+++ step_definitions/pages/google_page.rb
@@ -2,20 +2,39 @@ require 'rubygems'
 require 'selenium-webdriver'
 
 class Google_page
-  @@driver = Selenium::WebDriver.for :firefox, marionette: true
+  caps = Selenium::WebDriver::Remote::Capabilities.send("chrome")
+  #@@driver = Selenium::WebDriver.for :firefox, marionette: true
+  @@driver = Selenium::WebDriver.for(:remote, url: "http://192.168.99.100:4444/wd/hub", desired_capabilities: caps)
 
   def open_page
-      @page = "https://www.google.com/"
+
+    @page = "https://www.google.com/"
+
+    begin
       @@driver.navigate.to @page
+    rescue Exception => e
+      puts "There is an error to open the page #{@page}. #{e}"
+    end
   end
 
   def fill_search_bar(search)
-      @search_element = "lst-ib"
+
+    @search_element = "lst-ib"
+
+    begin
       @@driver.find_element(:id,"#@search_element").send_keys(search)
+      @@driver.save_screenshot('teste.png')
+    rescue Exception => e
+      puts "There is an error to fill search bar with the id #{@search_element}. #{e}"
+    end
   end
 
-  def click_pesquisa_google
-    @pesquisar_google_element = "btnK"
-    @@driver.find_element(:name, "#@pesquisar_google_element").click
+  def submit
+
+    begin
+      @@driver.action.send_keys("\n").perform
+    rescue Exception => e
+      puts "There is an error to submit the form. #{e}"
+    end
   end
 end
