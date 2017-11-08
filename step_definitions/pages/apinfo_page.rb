0ed150b0045d008fcc38a9dbebf1fe51fa3c7c3c
Daniel <danieldossantos1707@yahoo.com.br>
2017-11-08 11:56:20 +0000

    Changed class apinfo_page.rb to receive the method click

--- step_definitions/pages/apinfo_page.rb
+++ step_definitions/pages/apinfo_page.rb
@@ -2,5 +2,13 @@ require 'rubygems'
 require './core/driver'
 
 class ApInfo_page
-  
+  def pesquisar
+
+    begin
+
+    rescue Exception => e
+      puts "There is an error when click on"
+    end
+
+  end
 end
